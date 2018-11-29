25ff5e97a2117458e9887ab87f2e28d386353a91
eMariaR <elo-maria.roots@khk.ee>
2018-11-29 11:00:51 +0200

    paberikulu

--- pohimoisted/src/Printer.java
+++ pohimoisted/src/Printer.java
@@ -5,11 +5,19 @@ public class Printer {
 
     //nüüd vaja konstruktorit
     public Printer(Integer tooneriTase, Boolean kahepoolne) {
+        if(tooneriTase >=0 & tooneriTase >= 100) {
+            this.tooneriTase = tooneriTase;
+        } else if (tooneriTase > 100) {
+            this.tooneriTase = 100;
+        } else {
+            this.tooneriTase = 0;
+        }
         this.tooneriTase = tooneriTase;
         this.paberPrintimiseks = 100;
         this.kahepoolne = kahepoolne;
     }
-    
+
+    //getter toonerile
     public Integer getTooneriTase() {
         return tooneriTase;
     }
@@ -24,6 +32,39 @@ public class Printer {
         } else {
             return  -1;
         }
+    }
+
+    public Integer prindi(Integer lehtedeArv) {
+        Integer paberiKulu = lehtedeArv;
+        if (this.kahepoolne) {
+            paberiKulu = lehtedeArv / 2 + lehtedeArv % 2;
+            System.out.println("kahepoolne printimine");
+        }
+        if (paberiKulu > this.paberPrintimiseks) {
+            System.out.println("Printeris ei jätku paberit");
+            System.out.println("Lisa " + (paberiKulu - this.paberPrintimiseks) + "paberit juurde");
+            return 0;
+        } else {
+            this.paberPrintimiseks = this.paberPrintimiseks - paberiKulu;
+            return paberiKulu;
+        }
+    }
+
+    public Integer lisaPaber(Integer lehtedeArv) {
+        if (lehtedeArv > 0 & lehtedeArv < 500) {
+            this.paberPrintimiseks = this.paberPrintimiseks + lehtedeArv;
+            System.out.println("Printerisse on lisatud " + lehtedeArv + " paberit");
+        }
+            return  this.paberPrintimiseks:
+        }
+
+        this.paberPrintimiseks = this.paberPrintimiseks - paberiKulu;
+        return  paberiKulu;
+    }
+
+    // getter
+    public Integer getPaberPrintimiseks() {
+        return paberPrintimiseks;
     }
 }
 
